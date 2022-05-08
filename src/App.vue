5d91e5794eeeadd253cf06fdea10dd55ba230d67
BenPanyanil <benbackman54@gmail.com>
2022-05-08 13:37:19 +0300

    project start commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <body>
+
+    <section class="card-menu">
+      <section class="card-menu-section">
+        <div class="program-objects">
+          <span>Class +</span>
+          <span>Class instance +</span>
+          <span>Function +</span>
+        </div>
+      </section>
+      <section class="saved-solutions">
+
+      </section>
+    </section>
+
+    <section
+      class="card-pool"
+      ref="cardPool">
+      <card-template></card-template>
+    </section>
+
+  </body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardTemplate from "./components/CardTemplate.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    CardTemplate,
+  },
+  data() {
+    return {
+      cardPool: null,
+    }
+  },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  $white: #fcfcee;
+
+  * {
+    box-sizing: border-box;
+    font-size: 0.9rem;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  body {
+    position: relative;
+    display: flex;
+    margin: 0;
+    width: 100vw;
+    overflow-x: hidden;
+    background: lightgoldenrodyellow;
+  }
+
+  .card-menu {
+    position: sticky;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    display: inline-flex;
+    flex-direction: column;
+    background: #221f22;
+    height: 100vh;
+    width: 400px;
+    padding: 40px;
+    color: $white;
+  }
+
+  .card-pool {
+    display: inline-block;
+    background: #2d2a2e;
+    width: 100%;
+  }
+
+  .program-objects {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .program-objects span {
+    cursor: pointer;
+  }
+
+  .card-menu-section {
+    border-bottom: 1px solid $white;
+  }
 </style>
